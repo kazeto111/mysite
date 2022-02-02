3ae7ae2beb97ab7abbd69d493858866962814e2f
kazeto111 <kazeto123w@gmail.com>
2022-02-02 15:28:56 +0900

    move channels to top

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,13 +35,13 @@ AUTH_USER_MODEL = 'accounts.User'
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels',
     'briefSNS.apps.BriefsnsConfig',
     'accounts.apps.AccountsConfig',
 ]
