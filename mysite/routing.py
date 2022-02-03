838cdfa53958fb7196fc6e1f84372b86618782f0
kazeto111 <kazeto123w@gmail.com>
2022-02-03 17:08:32 +0900

    move channels to top

--- mysite/routing.py
+++ mysite/routing.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path( 'briefSNS/dm/<str:name1>/<str:name2>/', consumers.ChatConsumer.as_asgi() ),
+    path( 'briefSNS/dm/', consumers.ChatConsumer.as_asgi() ),
 ]
 print("websocketパターン", websocket_urlpatterns)
 print("そのタイプ", type(websocket_urlpatterns[0]))
