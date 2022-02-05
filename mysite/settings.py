140dbe216cf561cb55709bcf61916e3c908bcad4
kazeto111 <kazeto123w@gmail.com>
2022-02-06 01:12:05 +0900

    for inifinite redirect

--- mysite/settings.py
+++ mysite/settings.py
@@ -170,7 +170,7 @@ if path not in sys.path:
     sys.path.append(path)
 
 #以下セキュリティ対策用
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT=True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
